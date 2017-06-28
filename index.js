d2f2c3c6732f389a11641059cf5447cd93cc3550
Diego Silveira <diegossilveira@gmail.com>
2017-06-28 11:34:03 -0300

    New commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Encoding', 'gzip')
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.write('{"message": "ok"}', 'utf-8')
+  res.end()
+})
+server.listen(8000)
